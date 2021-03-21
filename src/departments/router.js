d8c870ecacf9308cf417e60423d3cde43f71e379
Drew Love <davidandrewlove84@gmail.com>
2021-03-21 16:04:32 -0400

    test

--- src/departments/router.js
+++ src/departments/router.js
@@ -24,15 +24,14 @@ endpointRouter
   // .get(checkJwt, (req, res, next) => {
   .route("/")
   .get((req, res, next) => {
-    res.json({ ok: true });
-    // const knexInstance = req.app.get("db");
-    // endpointService
-    //   // .getAllRows(knexInstance, req.params.app_user_id)
-    //   .getAllRows(knexInstance)
-    //   .then((rows) => {
-    //     res.json(rows.map(serializeRow));
-    //   })
-    //   .catch(next);
+    const knexInstance = req.app.get("db");
+    endpointService
+      // .getAllRows(knexInstance, req.params.app_user_id)
+      .getAllRows(knexInstance)
+      .then((rows) => {
+        res.json(rows.map(serializeRow));
+      })
+      .catch(next);
   })
   .post(jsonParser, checkJwt, (req, res, next) => {
     const { department_name } = req.body;
