fa41c9fdb80bc9ab4e4e2b8a288911a937c0119e
Drew Love <davidandrewlove84@gmail.com>
2021-03-21 13:06:08 -0400

    Test, bearer token

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(cors());
 //   }))
 app.use(helmet());
 
-app.use(validateBearerToken);
+// app.use(validateBearerToken);
 app.use("/api/departments", departmentsRouter);
 app.use("/api/roles", rolesRouter);
 app.use("/api/shifts", shiftsRouter);
