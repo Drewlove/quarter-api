9bbe52cd32c40a4543ba524c54fc7bd398ae5041
Drew Love <davidandrewlove84@gmail.com>
2021-03-23 11:07:46 -0400

    add ssl string to connection

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const { PORT, DATABASE_URL } = require("./config");
 
 const db = knex({
   client: "pg",
-  connection: DATABASE_URL,
+  // connection: DATABASE_URL,
+  connection: { connectionString: DATABASE_URL, ssl: true },
   useNullAsDefault: true,
 });
 
