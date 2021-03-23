2e9e8df1d274895fcca3b9b385bcda98abfc5b0d
Drew Love <davidandrewlove84@gmail.com>
2021-03-23 11:51:05 -0400

    Add conditional expression so ssl: { rejectUnauthorized: false } is only set for production environment

--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
 const knex = require("knex");
 const app = require("./app");
 const { PORT, DATABASE_URL } = require("./config");
+const { NODE_ENV } = require("./config");
 
-const db = knex({
-  client: "pg",
-  connection: {
-    connectionString: DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-  },
-  useNullAsDefault: true,
-});
+if (NODE_ENV === "production") {
+  db = knex({
+    client: "pg",
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    useNullAsDefault: true,
+  });
+} else {
+  db = knex({
+    client: "pg",
+    connection: {
+      connectionString: DATABASE_URL,
+    },
+    useNullAsDefault: true,
+  });
+}
 
 app.set("db", db);
 
