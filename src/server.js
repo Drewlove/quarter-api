4db6c2bfc6da9b3a79564aef61ac4d3a46333bf4
Drew Love <davidandrewlove84@gmail.com>
2021-03-23 11:12:40 -0400

    fix ssl

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ const { PORT, DATABASE_URL } = require("./config");
 const db = knex({
   client: "pg",
   // connection: DATABASE_URL,
-  connection: { connectionString: DATABASE_URL, ssl: true },
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
   useNullAsDefault: true,
 });
 
