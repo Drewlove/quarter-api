21f9cec7a755d4998d9b72b8adeb8f5afade1f26
Drew Love <davidandrewlove84@gmail.com>
2021-03-23 11:33:32 -0400

    test

--- src/server.js
+++ src/server.js
@@ -1,19 +1,15 @@
 const knex = require("knex");
 const app = require("./app");
-const { NODE_ENV } = require("./config");
 const { PORT, DATABASE_URL } = require("./config");
 
 const db = knex({
   client: "pg",
   connection: {
     connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
   },
   useNullAsDefault: true,
 });
-
-if (NODE_ENV === "production") {
-  db.connection.ssl = { rejectUnauthorized: false };
-}
 
 app.set("db", db);
 
